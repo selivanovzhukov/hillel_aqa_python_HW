5104cec9dd688a8418f4ce95d6de642eb2d1c355
selivanovzhukov <kirill.selivanov1@gmail.com>
2025-03-30 19:07:06 +0300

    removed elif

--- lesson_06/homework_06_1.py
+++ lesson_06/homework_06_1.py
@@ -1,7 +1,6 @@
 list_of_symbols = input('Enter some text:')
 unique_symbols = set(list_of_symbols)
 
-if len(unique_symbols) < 10:
-    print(False)
-elif len(unique_symbols) >= 10:
+if len(unique_symbols) >= 10:
     print(True)
+else: print(False
