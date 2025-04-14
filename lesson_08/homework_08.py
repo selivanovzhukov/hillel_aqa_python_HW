c9976e509e013bfa38171147a962b00861cfcb95
selivanovzhukov <kirill.selivanov1@gmail.com>
2025-04-14 17:36:20 +0300

    Homework_08

---
+++ lesson_08/homework_08.py
@@ -0,0 +1,16 @@
+
+def element_sum(list_of_elements: list):
+    """Summing elements in the list of strings"""
+    for item in list_of_elements:
+        try:
+            elements = (item.split(','))
+            sum_of_elements = sum(int(num) for num in elements)
+            print(sum_of_elements)
+        except ValueError:
+            print("Не можу це зробити")
+
+
+
+
+list_of_elements = ["1,2,3,4", "1,2,3,4,50", "qwerty1,2,3"]
+element_sum(list_of_elements)
