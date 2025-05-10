20720ddbb8eab07b00cf903c4c89930ce2a00b15
selivanovzhukov <kirill.selivanov1@gmail.com>
2025-05-10 14:09:58 +0300

    hw_10 refactor changed unittest on pytest edit logger settings

--- lesson_10/test_log_event.py
+++ lesson_10/test_log_event.py
@@ -1,61 +1,59 @@
-import unittest
-import os
+import pytest
 from lesson_10.homework_10 import log_event
-import logging
 
 LOG_FILE = 'login_system.log'
 
-class TestLogEvent(unittest.TestCase):
-
-    def setUp(self):
-        logger = logging.getLogger("log_event")
-        if logger.hasHandlers():
-            logger.handlers.clear()
-        try:
-            with open(LOG_FILE, 'w'):
-                pass
-        except FileNotFoundError:
-            pass
-
-    @staticmethod
-    def read_log_file():
-        with open(LOG_FILE, 'r') as file:
-            return file.read()
+class TestLogEvent:
 
     def test_log_event_success(self):
         log_event(username='username1', status='success')
-        log_content = self.read_log_file()
-        self.assertIn('INFO Login event - Username: username1, Status: success', log_content)
-        self.assertNotIn('WARNING', log_content)
-        self.assertNotIn('ERROR', log_content)
+        with open(LOG_FILE, 'r') as f:
+            log_content = f.read()
+        assert 'INFO - Login event - Username: username1, Status: success' in log_content
+        assert 'WARNING - Login event - Username: username1, Status: success' not in log_content
+        assert 'ERROR - Login event - Username: username1, Status: success' not in log_content
+        assert 'username1' in log_content
+        assert 'success' in log_content
 
     def test_log_event_expired(self):
         log_event(username='username2', status='expired')
-        log_content = self.read_log_file()
-        self.assertIn('WARNING Login event - Username: username2, Status: expired', log_content)
-        self.assertNotIn('INFO', log_content)
-        self.assertNotIn('ERROR', log_content)
+        with open(LOG_FILE, 'r') as f:
+            log_content = f.read()
+        assert 'WARNING - Login event - Username: username2, Status: expired' in log_content
+        assert 'INFO - Login event - Username: username2, Status: expired' not in log_content
+        assert 'ERROR - Login event - Username: username2, Status: expired' not in log_content
+        assert 'username2' in log_content
+        assert 'expired' in log_content
 
     def test_log_event_empty_username(self):
         log_event(username='', status='success')
-        log_content = self.read_log_file()
-        self.assertIn('INFO Login event - Username: , Status: success', log_content)
-        self.assertNotIn('WARNING', log_content)
-        self.assertNotIn('ERROR', log_content)
+        with open(LOG_FILE, 'r') as f:
+            log_content = f.read()
+        assert 'INFO - Login event - Username: , Status: success' in log_content
+        assert 'WARNING- Login event - Username: , Status: success' not in log_content
+        assert 'ERROR - Login event - Username: , Status: success' not in log_content
+        assert 'Username: ' in log_content
+        assert 'success' in log_content
 
     def test_log_event_empty_status(self):
         log_event(username='username3', status='')
-        log_content = self.read_log_file()
-        self.assertIn('ERROR Login event - Username: username3, Status: ', log_content)
-        self.assertNotIn('WARNING', log_content)
-        self.assertNotIn('INFO', log_content)
+        with open(LOG_FILE, 'r') as f:
+            log_content = f.read()
+        assert 'ERROR - Login event - Username: username3, Status: ' in log_content
+        assert 'WARNING - Login event - Username: username3, Status: ' not in log_content
+        assert 'INFO - Login event - Username: username3, Status: ' not in log_content
+        assert 'username3' in log_content
+        assert 'Status: ' in log_content
 
     def test_log_event_empty_all(self):
         log_event(username='', status='')
-        log_content = self.read_log_file()
-        self.assertIn('ERROR Login event - Username: , Status: ', log_content)
-        self.assertNotIn('WARNING', log_content)
-        self.assertNotIn('INFO', log_content)
+        with open(LOG_FILE, 'r') as f:
+            log_content = f.read()
+        assert 'ERROR - Login event - Username: , Status: ' in log_content
+        assert 'WARNING - Login event - Username: , Status: ' not in log_content
+        assert 'Username: ' in log_content
+        assert 'Status: ' in log_content
+
 
 if __name__ == '__main__':
-    unittest.main()
+    pytest.main()
