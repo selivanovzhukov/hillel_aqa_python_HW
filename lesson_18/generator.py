33dd9d9952a2baf04365f8767a93a2f2d4179cec
selivanovzhukov <kirill.selivanov1@gmail.com>
2025-05-30 17:10:18 +0300

    homework_18 generators

---
+++ lesson_18/generator.py
@@ -0,0 +1,20 @@
+def generate_even_nums(n):
+    for i in range(n + 1):
+        if i % 2 == 0:
+            yield i
+
+
+for num in generate_even_nums(10):
+    print("Even numbers till 10: ", num)
+
+
+def generate_fibonacci(n):
+    a = 0
+    b = 1
+    while a <= n:
+        yield a
+        a, b = b, a + b
+
+for num in generate_fibonacci(10):
+    print("Fibonacci numbers till 10: ", num)
+
