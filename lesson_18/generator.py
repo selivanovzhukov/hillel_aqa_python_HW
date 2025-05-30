971af5c28f510b195a7f8703a0a3507b95450131
selivanovzhukov <kirill.selivanov1@gmail.com>
2025-05-30 17:48:00 +0300

    homework_18 gen, comment

--- lesson_18/generator.py
+++ lesson_18/generator.py
@@ -4,8 +4,8 @@ def generate_even_nums(n):
             yield i
 
 
-for num in generate_even_nums(10):
-    print("Even numbers till 10: ", num)
+# for num in generate_even_nums(10):
+#     print("Even numbers till 10: ", num)
 
 
 def generate_fibonacci(n):
@@ -15,6 +15,6 @@ def generate_fibonacci(n):
         yield a
         a, b = b, a + b
 
-for num in generate_fibonacci(10):
-    print("Fibonacci numbers till 10: ", num)
+# for num in generate_fibonacci(10):
+#     print("Fibonacci numbers till 10: ", num)
 
